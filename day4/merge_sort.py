708b2e3c2cd873ac8868b110897f6fe0f6d9eec4
Aryan-saini1 <aryansaini23cs@rnsit.ac.in>
2025-02-21 00:28:13 +0530

    merge sort rectified

--- day4/merge_sort.py
+++ day4/merge_sort.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-# Sum of Odd placed Even Digits
-import sys
-
-def sum_of_odd_placed_even_digits(number):
-    sum1 = 0
-    sum2 = 0
-    flip = True
-    while number > 0:
-        digit = number % 10
-        number = number // 10
-        if digit % 2 == 0:
-            if flip:
-                sum1 += digit
-            else:
-                sum2 += digit
-        flip = not flip
-    if flip:
-        return sum2
-    return sum1
-
-input_number = int(sys.argv[1])
-sum_of_digits = sum_of_odd_placed_even_digits(input_number)
-
-print(f'Sum of Odd placed Even digits of {input_number} is {sum_of_digits}')
-=======
 def merge(numbers, low, mid, high):
     array1 = numbers[low : mid+1]
     array2 = numbers[mid+1 : high+1]
@@ -54,4 +28,3 @@ numbers = [int(num) for num in input().split()]
 
 merge_sort(numbers, 0, len(numbers)-1)
 print('Sorted Numbers are \n', numbers)
->>>>>>> 88ea2cf0ba99955422708c47807c543e254d0e82
